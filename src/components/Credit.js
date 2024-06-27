7a01105838c61ac13b3ac14399a38902bfc6f4ce
abd2108 <162757837+abd2108@users.noreply.github.com>
2024-06-27 16:17:15 +0530

    ii

---
+++ src/components/Credit.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Credit() {
+  return (
+    <div class="credit">
+    <p>
+      
+      <a 
+        > 
+       <a></a></a> 
+    </p>
+  </div>
+  );
+}
